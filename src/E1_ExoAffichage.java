fd7f520fc8206a201113ad0fae2b3e5ba9af913a
Christian Lisangola <jochri3@gmail.com>
2022-11-14 17:01:48 +0300

    lecture & ecriture

---
+++ src/E1_ExoAffichage.java
@@ -0,0 +1,10 @@
+public class E1_ExoAffichage {
+    public static void main(String[] args) {
+        double longueur=250;
+        double largeur=140;
+        double surface=longueur*largeur;
+        double perimetre=2*(longueur+largeur);
+        System.out.printf("longueur : %.1f m\nlargeur : %.1f\nsurface : %.1f m2" +
+                "\npérimètre : %.1f m",longueur,largeur,surface,perimetre);
+    }
+}
---
+++ src/E2_LectureClavier.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class E2_LectureClavier {
+    public static void main(String[] args) {
+        Scanner keyboardInput=new Scanner(System.in);
+        System.out.print("Votre nom : ");
+        String nom=keyboardInput.nextLine();
+
+        System.out.print("Votre prenom : ");
+        String prenom=keyboardInput.nextLine();
+
+        System.out.print("Votre age : ");
+        int age=keyboardInput.nextInt();
+
+
+        System.out.print("Votre poids : ");
+        double poids=keyboardInput.nextDouble();
+
+
+        System.out.printf("Nom : %s\nPrenom : %s\nAge : %d ans\nPoids : %.1f kg",nom,
+                prenom,age
+                ,poids);
+
+    }
+}
